08d635aa033bb2f23eede91ebf9a0706b658ceac
pajaro2113 <alvarojavierpajarofernandez@gmail.com>
2026-06-11 05:46:48 -0500

    login and verificly

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     launchOptions: {
-      slowMo: 2000 //poner mas lenta la ejecucion
+      slowMo: 1000 //poner mas lenta la ejecucion
     }
   },
 
@@ -67,10 +67,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+     {
+       name: 'Google Chrome',
+       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+     },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/reto.spec.ts
+++ tests/reto.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test'
 
-test('login page', async ({ page }) => {
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+test('login to hrm', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
     await page.getByRole('textbox', {name:'Username'}).fill('Admin')
-    await page.getByRole('textbox', {name:'password'}).fill('admin123x  ')
-    await page.getByRole('button', {name:'Login'}).click
+    await page.getByRole('textbox', {name:'password'}).fill('admin123')
+    await page.getByRole('button', {name:'Login'}).click()
+    await expect(page.getByRole('heading', {name:'Dashboard'})).toBeVisible()
 
 })
