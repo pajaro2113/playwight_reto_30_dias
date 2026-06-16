d32f9ad4866199c50093afd9b053549c14c2dcbb
pajaro2113 <alvarojavierpajarofernandez@gmail.com>
2026-06-16 18:17:49 -0500

    get all the columm -> employee name

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -29,3 +29,30 @@ test('Get all the usernames registred', async ({ page }) => {
     }
     console.log(usernames)
 })
+
+test('get all the Employee Name', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', {name:'Username'}).fill('Admin')
+    await page.getByRole('textbox', {name:'password'}).fill('admin123')
+    await page.getByRole('button', {name:'Login'}).click()
+
+    await expect(page.getByRole('link', {name:'Admin'})).toBeVisible()
+
+    await page.getByRole('link', {name:'Admin'}).click()
+
+    await page.getByRole('navigation', {name:'Topbar Menu'}).getByText('User Management ').click()
+    await page.getByRole('menuitem', {name:'Users'}).click()
+
+    const rows = page.getByRole('table').getByRole('row')
+    const usernames: string[] = []
+    const rowcount = await rows.count()
+    for(let i=1; i<rowcount; i++){
+        const cell = rows.nth(i).getByRole('cell').nth(3)
+        const username = await cell.textContent()
+        if(username){
+            usernames.push(username)
+        }
+    }
+    console.log(usernames)
+
+})
